a38d299177225b4eb44aa4d2c97aa0eed8a1856f
augustinaviciusR <31822882+augustinaviciusR@users.noreply.github.com>
2023-09-01 12:59:07 +0300

    RetryableHttpClient TestCases added

--- src/main/kotlin/lt/homeassignment/jokesapplication/model/Joke.kt
+++ src/main/kotlin/lt/homeassignment/jokesapplication/model/Joke.kt
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonProperty
 import java.time.LocalDateTime
 
+class JokeApiRateLimitException(message: String) : RuntimeException(message)
+class JokeApiBadRequestException(message: String) : RuntimeException(message)
 class JokeApiException(message: String) : RuntimeException(message)
 
 data class JokeSearchResult(
