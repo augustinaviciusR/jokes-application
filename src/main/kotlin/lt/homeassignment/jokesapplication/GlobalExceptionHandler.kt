a3cbf557e4bf98ee1ae7127c889b2cef368e9780
augustinaviciusR <31822882+augustinaviciusR@users.noreply.github.com>
2023-09-02 09:06:35 +0300

    updated docker-compose.yaml to build image if does not exist

--- src/main/kotlin/lt/homeassignment/jokesapplication/GlobalExceptionHandler.kt
+++ src/main/kotlin/lt/homeassignment/jokesapplication/GlobalExceptionHandler.kt
@@ -1,5 +1,7 @@
 package lt.homeassignment.jokesapplication
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import jakarta.validation.ConstraintViolationException
 import lt.homeassignment.jokesapplication.model.JokeApiException
 import lt.homeassignment.jokesapplication.model.JokeApiRateLimitException
 import mu.KotlinLogging
@@ -7,12 +9,13 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.ServletWebRequest
 
 // Configure a global exception handler to handle exceptions thrown by the application
 // This way we can handle exceptions thrown by the application in a single place
 // And logs won't be polluted with stacktraces
 @ControllerAdvice
-class GlobalExceptionHandler {
+class GlobalExceptionHandler(private val objectMapper: ObjectMapper) {
 
     private val logger = KotlinLogging.logger {}
 
@@ -28,9 +31,33 @@ class GlobalExceptionHandler {
         return ResponseEntity(ex.message, HttpStatus.TOO_MANY_REQUESTS)
     }
 
+    @ExceptionHandler(ConstraintViolationException::class)
+    fun handleConstraintViolationException(
+        exception: ConstraintViolationException,
+        webRequest: ServletWebRequest
+    ) {
+        val response = webRequest.response!!
+        response.status = HttpStatus.BAD_REQUEST.value()
+        response.contentType = "application/json"
+
+        val errorResponse = ErrorResponse(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = "Bad Request",
+            message = exception.message ?: "Validation error"
+        )
+
+        val json = objectMapper.writeValueAsString(errorResponse)
+        response.writer.write(json)
+    }
+
     @ExceptionHandler(Exception::class)
     fun handleGenericException(ex: Exception): ResponseEntity<String> {
         logger.error("An error occurred: {}", ex.message)
         return ResponseEntity("Internal Server Error", HttpStatus.INTERNAL_SERVER_ERROR)
     }
 }
+data class ErrorResponse(
+    val status: Int,
+    val error: String,
+    val message: String
+)
